dabd28253889e67ea5635d3e6b49f7f41c01c4a9
Gourav Chouhan <gg0uravgch0uhan@gmail.com>
2023-10-21 13:53:00 +0530

    updated some info

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-//geting dimentions
-
-// const width = window.innerWidth
-// const height = window.innerHeight
-
-// const mobile_width = 600
-
-// let out;
-
-// if (width < mobile_width) {
-//     console.log(width)
-//     let stuffs_ = document.querySelectorAll('.stuffs');
-//     out = stuffs_[0]
-//     // console.log(stuffs_)
-//     stuffs_.forEach(elm => elm.style.flexDirection = 'column')
-// }
-
-// window.addEventListener('click', e => {
-//     // let elm = document.querySelector('.firstLook')
-//     let a = document.getElementById('test1');
-//     let b = document.getElementById('test2');
-    
-//     a.classList.toggle('aajo')
-//     b.classList.toggle('aajo')
-// })
 
 let observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -43,21 +18,6 @@ g.forEach(_g => observer.observe(_g))
 
 
 
-
-//for tilting just add tilt class to a elemnt and set some parameter
-// for tilting the card
-
-
-// let tilt = document.querySelectorAll('.tilt')
-// let tiltY = 0.01;
-// let tiltX = 0;
-// tilt.forEach(_card => _card.addEventListener('mousemove', e => {
-//   let x = e.clientX - _card.offsetLeft - _card.offsetWidth / 2;
-//     let y = e.clientY - _card.offsetTop - _card.offsetHeight / 2;
-//     console.log(_card);
-  
-//   tilt.forEach(_element => _element.style.transform = `rotateX(${y * tiltX/10}deg) rotateY(${x * tiltY/10}deg)`)
-// }))
 
 
 document.querySelector('.hamburger').addEventListener('click', () => {
--- typing-animation.js
+++ typing-animation.js
@@ -2,12 +2,12 @@ const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
 const textArray = [
-	"Gourav Chouhan",
-	"currently in my 3rd year pursuing B.Tech",
-	"a Problem Solver",
-	"a Web Devloper",
-	"a Deep Thinker",
-	"a Funny guy",
+  "Gourav Chouhan",
+  "currently in my 4th year pursuing B.Tech",
+  "a Problem Solver",
+  "a Web Devloper",
+  "a Deep Thinker",
+  "a Funny guy",
 ];
 const typingDelay = 80;
 const erasingDelay = 40;
@@ -16,37 +16,37 @@ let textArrayIndex = 0;
 let charIndex = 0;
 
 function type() {
-	if (charIndex < textArray[textArrayIndex].length) {
-		if (!cursorSpan.classList.contains("typing"))
-			cursorSpan.classList.add("typing");
-		typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-		charIndex++;
-		setTimeout(type, typingDelay);
-	} else {
-		cursorSpan.classList.remove("typing");
-		setTimeout(erase, newTextDelay);
-	}
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
 }
 
 function erase() {
-	if (charIndex > 0) {
-		if (!cursorSpan.classList.contains("typing"))
-			cursorSpan.classList.add("typing");
-		typedTextSpan.textContent = textArray[textArrayIndex].substring(
-			0,
-			charIndex - 1
-		);
-		charIndex--;
-		setTimeout(erase, erasingDelay);
-	} else {
-		cursorSpan.classList.remove("typing");
-		textArrayIndex++;
-		if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-		setTimeout(type, typingDelay + 1100);
-	}
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing"))
+      cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-	// On DOM Load initiate the effect
-	if (textArray.length) setTimeout(type, newTextDelay + 250);
+  // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
